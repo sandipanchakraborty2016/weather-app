9622fcd66c4279c3767d5c38dc50e90e7887bb11
Sandipan <sandipanchakraborty@live.in>
2023-10-03 19:53:17 +0530

    Adding exception handlers and test cases

--- weather-clients/src/test/java/com/weather/clients/services/impl/ApiWeatherServiceImplTest.java
+++ weather-clients/src/test/java/com/weather/clients/services/impl/ApiWeatherServiceImplTest.java
@@ -1,11 +1,15 @@
 package com.weather.clients.services.impl;
 
 import com.weather.clients.config.ApiClientConfiguration;
+import com.weather.commons.constants.ErrorConstants;
+import com.weather.commons.exceptions.BusinessException;
 import com.weather.models.Root;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ApiWeatherServiceImplTest {
 
@@ -16,8 +20,14 @@ class ApiWeatherServiceImplTest {
         apiWeatherService = new ApiWeatherServiceImpl(new ApiClientConfiguration().webClient());
     }
     @Test
-    void fetchForecastForUpcomingDays() {
+    void valid_fetchForecastForUpcomingDays() {
         Root london = apiWeatherService.fetchForecastForUpcomingDays( "London",3).block();
         assertThat(london).isNotNull();
+    }
+
+    @Test
+    void fetchForecastForUpcomingDays() {
+        BusinessException correct = assertThrows(BusinessException.class, ()->apiWeatherService.fetchForecastForUpcomingDays( "London123",3).block());
+        assertEquals(ErrorConstants.BUSINESS_EXCEPTION.getErrorMessage(), correct.getApiWeatherError().getErrorMessage());
     }
 }
--- weather-commons/src/main/java/com/weather/commons/exceptions/CustomControllerAdvice.java
+++ weather-commons/src/main/java/com/weather/commons/exceptions/CustomControllerAdvice.java
@@ -6,8 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.time.LocalDateTime.now;
 
 @RestControllerAdvice
 @Slf4j
@@ -23,6 +26,20 @@ public class CustomControllerAdvice {
         errorMap.put("TIMESTAMP", businessException.getApiWeatherError().getTimestamp());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
+                .body(errorMap);
+    }
+
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleBookAPIException(RuntimeException runtimeException) {
+        Map<String, String> errorMap = new HashMap<>();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        log.info("Unknown exception has occurred", runtimeException);
+        errorMap.put("MESSAGE", "UNKNOWN_EXCEPTION_PLEASE CONTACT CUSTOMER CARE.");
+        errorMap.put("STATUS", HttpStatus.INTERNAL_SERVER_ERROR.toString());
+        errorMap.put("TIMESTAMP", dateTimeFormatter.format(now()));
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(errorMap);
     }
 }
